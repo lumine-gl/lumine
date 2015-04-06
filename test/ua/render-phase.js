ae22f666bea828915023f70205146550600b65d5
Will Shown <w@willshown.com>
2015-04-06 15:13:17 -0700

    Using Three because it's easier.

--- test/ua/render-phase.js
+++ test/ua/render-phase.js
@@ -3,7 +3,7 @@ var l = new Lumine();
 describe('Lumine Canvas', function(){
 
   it('should have a WebGL context.', function(){
-    return l.Canvas.gl.canvas.should.equal(l.Canvas.el);
-  })
+    return l.Canvas.renderer.domElement.should.equal(l.Canvas.el);
+  });
 
 });
