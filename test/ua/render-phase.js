4dca61d801ce694bb6d000994023719eec8adf8a
Will Shown <w@willshown.com>
2015-04-07 17:15:20 -0700

    Layout is rendering, but there are a couple issues.

--- test/ua/render-phase.js
+++ test/ua/render-phase.js
@@ -8,16 +8,37 @@ describe('Lumine Canvas', function(){
 
   it('should render a layout.', function(){
     l.layout({
+      typography: {
+        base: 16 // CSS pixels
+      },
       segments: [
         {
-          region: '1'
+          region: '1',
+          page: {
+            'max-columns': 4, // number
+            'min-columns': 1, // number
+            'min-horizontal-margin': 2, // base type size
+            'vertical-margin': 1, // base type size
+            'min-height': 30, // base type size
+            'max-height': 40, // base type size
+            columns: {
+              gutter: 1, // base type size
+              'min-width': 18, // base type size
+              'max-width': 24 // base type size
+            },
+            modules: {
+              'min-height': 10,
+              'gutter': 1
+            }
+          }
         }
       ],
       modules: {
         1: [
-          {
-            fill: 0x000000
-          }
+          { fill: 0x252321 },
+          { fill: 0x393432 },
+          { fill: 0x6A7F7A },
+          { fill: 0x393432 }
         ]
       }
     });
