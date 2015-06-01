c2c1f343261cc9b635881500ecd3ea7809489a6e
Will Shown <w@willshown.com>
2015-06-01 15:53:19 -0700

    Changing the API.

--- test/lib-test.js
+++ test/lib-test.js
@@ -1,13 +1,11 @@
-var l = new Lumine();
-
 describe('Lumine library', function(){
 
   it('should have certain keys', function(){
 
-    return l.should.have.property('Canvas') &&
-           l.should.have.property('Composer') &&
-           l.should.have.property('Passes') &&
-           l.should.have.property('Shaders');
+    return Lumine.should.have.property('Canvas') &&
+           Lumine.should.have.property('Composer') &&
+           Lumine.should.have.property('Passes') &&
+           Lumine.should.have.property('Shaders');
 
   });
 
