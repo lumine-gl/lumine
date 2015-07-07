777810d5114b0f0221e0029dffa7f8cca7e10e85
Will Shown <w@willshown.com>
2015-07-07 11:51:43 -0700

    This provides a general solution for UV issues.

--- lib/shaders/add-2d/add-2d.fragment.glsl
+++ lib/shaders/add-2d/add-2d.fragment.glsl
@@ -3,13 +3,15 @@ uniform sampler2D t1;
 
 uniform vec2 cUv0;
 uniform vec2 cUv1;
+uniform vec2 oUv0;
+uniform vec2 oUv1;
 
 varying vec2 vUv;
 
 void main() {
 
-  vec4 texel0 = texture2D( t0, vUv * cUv0 );
-  vec4 texel1 = texture2D( t1, vUv * cUv1 );
+  vec4 texel0 = texture2D( t0, oUv0 + vUv * cUv0 );
+  vec4 texel1 = texture2D( t1, oUv1 + vUv * cUv1 );
 
   gl_FragColor = min(texel0 + texel1, vec4(1.0, 1.0, 1.0, 1.0));
 
